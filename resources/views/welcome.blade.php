02e9d691897b9e87d6bdc458aaeb124ee361028d
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2023-12-13 15:27:09 -0300

    design draft #1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
 
         <title>Laravel</title>
 
@@ -12,10 +13,35 @@
 
     </head>
     <body class="antialiased">
-
-        <nav></nav>
-        <main></main>
-        <footer></footer>
         
+        <nav class="w-screen h-10 bg-slate-400 flex justify-between items-center">
+            <div>
+                <h3 class="ml-5">Titulo</h3>
+            </div>
+            <div>
+                <ul class="flex space-x-4 mr-5">
+                    <li>Login</li>
+                    <li>Logout</li>
+                    <li>Reset password</li>
+                </ul>
+            </div>
+        </nav>
+
+        <main>
+            <div class="w-[10rem] h-screen flex flex-col items-center bg-slate-400 fixed">
+                <div class="bg-orange-400 mt-5 self-start pl-5 pr-2">Dashboard</div>
+                <div>
+                    <ul>
+                        <li>Perfil</li>
+                        <li>Opcion 1</li>
+                        <li>Opcion 2</li>
+                        <li>Opcion 3</li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+
+        <footer></footer>
+
     </body>
 </html>
