06a222823f7dd8a63b1db57bef5eb86188c535e3
unknown <fabricio.rivera2012@gmail.com>
2023-12-15 00:05:46 -0300

    design draft #3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,26 +12,24 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     </head>
-    <body class="antialiased">
-        
-        <nav class="w-screen h-10 bg-zinc-200 flex justify-between items-center">
-            <div>
-                <h3 class="ml-4 text-2xl font-bold text-slate-600">TITULO</h3>
-            </div>
-            <div>
-                <ul class="flex space-x-4 mr-5 text-sm">
-                    <li><a href="#">Login</a></li>
-                    <li><a href="#">Logout</a></li>
-                    <li><a href="#">Reset password</a></li>
-                </ul>
-            </div>
-        </nav>
+    <body class="antialiased w-screen">
 
-        <main>
-            <div class="w-[10rem] h-screen flex flex-col items-center bg-slate-400 fixed">
-                <div class="bg-slate-200 mt-16 self-start pl-5 pr-2 font-semibold">Dashboard</div>
+        <main class="flex">
+            <div class="w-full fixed top-0">
+                <nav class="bg-slate-100 h-14">
+                    <ul class="flex justify-end space-x-5 pr-5 text-sm h-full items-center text-slate-600 shadow-md w-full whitespace-nowrap">
+                        <div class="w-full self-start pl-5 py-3 font-bold text-xl text-slate-700">Dashboard</div>
+                        <li>Usuario: ---</li>
+                        <li>Rol: ----
+                            <select name="" id=""></select> {{-- a modo de demostracion --}}
+                        </li>
+                        <li>Salir</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="w-[10rem] h-screen flex flex-col items-center bg-slate-700 z-50 shadow-slate-900 fixed top-14">
                 <div class="w-24">
-                    <ul class="space-y-4 mt-4 text-slate-100">
+                    <ul class="space-y-4 mt-4 text-slate-100 whitespace-nowrap">
                         <li><a href="#">Perfil</a></li>
                         <li><a href="#">Opcion</a></li>
                         <li><a href="#">Opcion</a></li>
