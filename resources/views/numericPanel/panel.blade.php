cbfd586c3184d4f93c3b1dd26e26d088baa7903b
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2023-12-28 11:21:28 -0300

    añadiendo diseño de panel para pedir numero draft#1

--- resources/views/numericPanel/panel.blade.php
+++ resources/views/numericPanel/panel.blade.php
@@ -1,18 +1,20 @@
 <x-layout>
 
     <div class="w-screen h-screen flex justify-center items-center">
-        <div class="grid grid-cols-3 grid-rows-4 ">
-            <div class="col-span-1">1</div>
-            <div class="col-span-1">2</div>
-            <div class="col-span-1">3</div>
-            <div class="col-span-1">4</div>
-            <div class="col-span-1">5</div>
-            <div class="col-span-1">6</div>
-            <div class="col-span-1">7</div>
-            <div class="col-span-1">8</div>
-            <div class="col-span-1">9</div>
-            <div class="col-span-3">0</div>
-        </div>
+        <form class="grid grid-cols-3 grid-rows-4 gap-1">
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">1</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">2</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">3</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">4</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">5</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">6</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">7</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">8</div>
+            <div class="col-span-1 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300">9</div>
+            <div class="col-span-3 p-5 bg-slate-300 border font-semibold text-slate-500 hover:bg-blue-300 text-center">0</div>
+
+            <button class="border rounded-xs bg-blue-400 w-full col-span-3 py-1 font-semibold text-slate-100">Siguiente</button>
+        </form>
     </div>
 
 </x-layout>
