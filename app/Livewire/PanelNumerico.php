d61adc56db4917bcc1f0edbd07d164c022d6bc9e
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2024-01-09 15:53:32 -0300

    añadiendo funcion de multiples cedulas

--- app/Livewire/PanelNumerico.php
+++ app/Livewire/PanelNumerico.php
@@ -8,11 +8,11 @@ use Livewire\Component;
 
 class PanelNumerico extends Component
 {
-    public $numero = '';
+    public $displayNumber = ''; //el numero que esta en el panel
 
-    public $displayNumber = '';
+    public $customers_id = ''; //el valor de la cedula que se ingreso
 
-    public $customers_id = '';
+    public $manyCustomers = []; //si se ingresan varias cedulas se agregan aqui
 
     public $estados_id = 1;
 
@@ -32,48 +32,57 @@ class PanelNumerico extends Component
 
     public function clear()
     {
+        //limpiar el array del customers
+        $this->manyCustomers = [];
         //se limpia el numero que este en el panel
         $this->displayNumber = '';
     }
     
-    public function save()
+    public function add()
     {
         //se setea la variable customer_id con la cedula que halla ingresado el usuario en el panel
         $this->customers_id = intval($this->displayNumber);
 
-        // Clear the display for a new number
+        // limpia el display para ingresar otra cedula
         $this->displayNumber = '';
-        $this->dispatch('error');
+        // $this->dispatch('error');
         
+        //validaciones
         $validated = $this->validate([
-            'customers_id' => 'exists:customers,ci', //valido que exista la cedula
-            'estados_id' => 'required',
-            'filas_id' => 'required'
+            'customers_id' => 'exists:customers,ci',
+            'estados_id' => 'required'
         ]);
-
-        $number = Numeros::create([
-            'numero' => (Numeros::latest()->first()) ? Numeros::latest()->orderBy('id', 'desc')->first()->numero + 1 : 1,
-            'estados_id' => 1,
-            'filas_id' => 1
+        //se inserta al array de customers despues de la validacion de que existen
+        array_push($this->manyCustomers, [
+            'ci' => $this->customers_id, 'name' => Customers::where('ci', $this->customers_id)->get('name')[0]->name
         ]);
+        $this->dispatch('numberAdded');
 
-        //si el numero se crea, entonces se le asigna el numero al customer que halla ingresado su cedula
-        if(isset($number)){
-            Customers::where('ci', $validated['customers_id'])->update([
-                'numeros_id' => Numeros::latest()->orderBy('id', 'desc')->first()->id
-            ]);
-        }else{
-            //sino se envia un error al panel
-            $this->dispatch('error');
-        }
+        // //creo el numero
+        // $number = Numeros::create([
+        //     'numero' => (Numeros::latest()->first()) ? Numeros::latest()->orderBy('id', 'desc')->first()->numero + 1 : 1,
+        //     'estados_id' => 1
+        // ]);
+
+        // //si el numero se crea, entonces se le asigna el numero al customer que halla ingresado su cedula
+        // if(isset($number)){
+        //     Customers::where('ci', $validated['customers_id'])->update([
+        //         'numeros_id' => Numeros::latest()->orderBy('id', 'desc')->first()->id
+        //     ]);
+        // }else{
+        //     //sino se envia un error al panel
+        //     $this->dispatch('error');
+        // }
         
 
-        $this->dispatch('numberCreated');
+        // $this->dispatch('numberCreated');
     }
 
 
     public function render()
     {
-        return view('livewire.panel-numerico');
+        return view('livewire.panel-numerico', [
+            'cedulas' => $this->manyCustomers
+        ]);
     }
 }
--- resources/views/livewire/panel-numerico.blade.php
+++ resources/views/livewire/panel-numerico.blade.php
@@ -1,6 +1,5 @@
 <div>
-    <div class="w-screen h-screen flex flex-col justify-start items-center pt-20" x-data="{'isModalOpen': false}" x-on:keydown.escape="isModalOpen=false">
-
+    <div class="w-screen h-screen flex flex-col justify-start items-center pt-20">
         <h1 class="text-xl uppercase font-semibold text-slate-500 mb-1">Ingrese su documento</h1>
         <h2 class="text-sm font-semibold text-slate-500 mb-10">Sin puntos ni guiones</h2>
 
@@ -12,100 +11,123 @@
             <div wire:click="deleteNumber" class="col-span-1 p-5 text-4xl bg-orange-300 border font-semibold text-slate-500 hover:bg-blue-300 text-center"><</div>
         </div>
 
-        <input class="my-5 px-5 py-2 border rounded-sm" type="text" wire:model="displayNumber" readonly>
+        <input class="my-5 px-5 py-2 border rounded-sm w-3/5 max-w-sm antialiased text-slate-400 text-center font-semibold bg-slate-50" type="text" wire:model="displayNumber" readonly>       
+        
+        {{-- Error message --}}
+        <div class="fixed inset-x-0 bottom-10 flex items-center justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-50">
+            <div    
+                x-data="{ show: false, timeout: null }"
+                x-init="@this.on('error', () => {
+                    clearTimeout(timeout);
+                    show = true;
+                    timeout = setTimeout(() => show = false, 5000);
+                })"
+                x-show="show" 
+                x-description="Notification panel, show/hide based on alert state." 
+                x-transition:enter="transition ease-out duration-300"
+                x-transition:enter-start="opacity-0 transform scale-90"
+                x-transition:enter-end="opacity-100 transform scale-100"
+                x-transition:leave="transition ease-in duration-300"
+                x-transition:leave-start="opacity-100 transform scale-100"
+                x-transition:leave-end="opacity-0 transform scale-90"
+                class="max-w-xs w-full bg-white shadow-lg rounded-lg pointer-events-auto"
+            >
+                <div class="rounded-lg shadow-xs overflow-hidden bg-red-500">
+                    <div class="p-4">
+                        <div class="flex items-start">
+                            <div class="ml-3 w-0 flex-1 pt-0.5">
+                                <p class="font-semibold leading-5 text-gray-100">
+                                    No se encontro el documento
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        {{-- Info message for adding number --}}
+        <div class="fixed inset-x-0 bottom-10 flex items-center justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-50">
+            <div 
+                x-data="{ show: false, timeout: null, numberOK: false }"
+                x-init="@this.on('numberAdded', () => {
+                    clearTimeout(timeout);
+                    show = true;
+                    timeout = setTimeout(() => show = false, 3000);
+                    numberOK = true;
+                })"
+                x-show="show" 
+                x-description="Notification panel, show/hide based on alert state." 
+                x-transition:enter="transition ease-out duration-300"
+                x-transition:enter-start="opacity-0 transform scale-90"
+                x-transition:enter-end="opacity-100 transform scale-100"
+                x-transition:leave="transition ease-in duration-300"
+                x-transition:leave-start="opacity-100 transform scale-100"
+                x-transition:leave-end="opacity-0 transform scale-90"
+                class="max-w-xs w-full bg-white shadow-lg rounded-lg pointer-events-auto"
+            >
+                <div class="rounded-lg shadow-xs overflow-hidden bg-blue-500">
+                    <div class="p-4">
+                        <div class="flex items-start">
+                            <div class="ml-3 w-0 flex-1 pt-0.5">
+                                <p class="font-semibold text-center leading-5 text-gray-100">
+                                    Numero agregado
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        <div> 
-            <div
-                class="modal"
-                role="dialog"
-                tabindex="-1"
-                x-show="isModalOpen"
-                x-on:click.away="isModalOpen = false"
-                x-cloak
-                x-transition
-                >
-                <div>
-                    <div>
-                        <h3>Hello World</h3>
-                        <button aria-label="Close" x-on:click="isModalOpen=false">✖</button>
+        {{-- Info message for creating number --}}
+        <div class="fixed inset-x-0 bottom-10 flex items-center justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-50">
+            <div 
+                x-data="{ show: false, timeout: null, numberOK: false }"
+                x-init="@this.on('numberCreated', () => {
+                    clearTimeout(timeout);
+                    show = true;
+                    timeout = setTimeout(() => show = false, 3000);
+                    numberOK = true;
+                })"
+                x-show="show" 
+                x-description="Notification panel, show/hide based on alert state." 
+                x-transition:enter="transition ease-out duration-300"
+                x-transition:enter-start="opacity-0 transform scale-90"
+                x-transition:enter-end="opacity-100 transform scale-100"
+                x-transition:leave="transition ease-in duration-300"
+                x-transition:leave-start="opacity-100 transform scale-100"
+                x-transition:leave-end="opacity-0 transform scale-90"
+                class="max-w-xs w-full bg-white shadow-lg rounded-lg pointer-events-auto"
+            >
+                <div class="rounded-lg shadow-xs overflow-hidden bg-green-500">
+                    <div class="p-4">
+                        <div class="flex items-start">
+                            <div class="ml-3 w-0 flex-1 pt-0.5">
+                                <p class="font-semibold text-center leading-5 text-gray-100">
+                                    Imprimiendo numero
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div>Quiere ingresar otra cedula?</div>
                 </div>
             </div>
         </div>
         
-        @error('customers_id')
-            <div class="fixed inset-x-0 bottom-10 flex items-center justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-50">
-                <div    
-                    x-data="{ show: false, timeout: null }"
-                    x-init="@this.on('error', () => {
-                        clearTimeout(timeout);
-                        show = true;
-                        timeout = setTimeout(() => show = false, 5000);
-                    })"
-                    x-show="show" 
-                    x-description="Notification panel, show/hide based on alert state." 
-                    x-transition:enter="transition ease-out duration-300"
-                    x-transition:enter-start="opacity-0 transform scale-90"
-                    x-transition:enter-end="opacity-100 transform scale-100"
-                    x-transition:leave="transition ease-in duration-300"
-                    x-transition:leave-start="opacity-100 transform scale-100"
-                    x-transition:leave-end="opacity-0 transform scale-90"
-                    class="max-w-xs w-full bg-white shadow-lg rounded-lg pointer-events-auto"
-                >
-                    <div class="rounded-lg shadow-xs overflow-hidden bg-red-500">
-                        <div class="p-4">
-                            <div class="flex items-start">
-                                <div class="ml-3 w-0 flex-1 pt-0.5">
-                                    <p class="font-semibold leading-5 text-gray-100">
-                                        No se encontro el documento
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @else
-            <div class="fixed inset-x-0 bottom-10 flex items-center justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-50">
-                <div 
-                    x-data="{ show: false, timeout: null, numberOK: false }"
-                    x-init="@this.on('numberCreated', () => {
-                        clearTimeout(timeout);
-                        show = true;
-                        timeout = setTimeout(() => show = false, 3000);
-                        numberOK = true;
-                    })"
-                    x-show="show" 
-                    x-description="Notification panel, show/hide based on alert state." 
-                    x-transition:enter="transition ease-out duration-300"
-                    x-transition:enter-start="opacity-0 transform scale-90"
-                    x-transition:enter-end="opacity-100 transform scale-100"
-                    x-transition:leave="transition ease-in duration-300"
-                    x-transition:leave-start="opacity-100 transform scale-100"
-                    x-transition:leave-end="opacity-0 transform scale-90"
-                    class="max-w-xs w-full bg-white shadow-lg rounded-lg pointer-events-auto"
-                >
-                    <div class="rounded-lg shadow-xs overflow-hidden bg-green-500">
-                        <div class="p-4">
-                            <div class="flex items-start">
-                                <div class="ml-3 w-0 flex-1 pt-0.5">
-                                    <p class="font-semibold text-center leading-5 text-gray-100">
-                                        Imprimiendo numero
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @enderror
-        
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-1 w-3/5 max-w-sm">
             <button wire:click="clear" class="border rounded-xs bg-red-400 px-3 col-span-3 py-1 font-semibold text-slate-100">Cancelar</button>
-            <button wire:click="clear" class="border rounded-xs bg-blue-400 px-3 col-span-3 py-1 font-semibold text-slate-100">Agregar otra cedula</button>
-            <button wire:click="save"  class="border rounded-xs bg-blue-400 px-3 col-span-3 py-1 font-semibold text-slate-100">Sacar numero</button>
+            <button wire:click="add"  class="border rounded-xs bg-blue-400 px-3 col-span-3 py-1 font-semibold text-slate-100">Siguiente</button>
         </div>
         
+        <div x-data="{ showFinalizar: @json(count($cedulas) > 0) }">
+            @forelse ($cedulas as $cedula)
+            <div class="w-3/5 max-w-sm flex items-center justify-center bg-slate-100">
+                <p class="text-slate-500 "><span class="text-slate-500">{{$cedula['ci']}} - {{$cedula['name']}}</span></p>
+            </div>
+            @empty
+            <p></p>
+            @endforelse
+            <button wire:click="save" x-show="showFinalizar" class="w-3/5 max-w-sm border rounded-xs bg-blue-500 px-3 col-span-3 py-1 my-3 font-bold text-slate-100 text-lg">Finalizar</button>
+        </div>
     </div>
 </div>
