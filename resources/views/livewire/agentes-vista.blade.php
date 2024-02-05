bce7f40d643fe2428e79475d5a63673e279d5cd8
Fabricio Rivera <fabricio.rivera2012@gmail.com>
2024-02-05 13:44:45 -0300

    arreglando panel numerico al ingresar cedula 1

--- resources/views/livewire/agentes-vista.blade.php
+++ resources/views/livewire/agentes-vista.blade.php
@@ -2,7 +2,6 @@
     <table class="min-w-full text-left text-sm font-light">
         <thead class="border-b font-medium dark:border-neutral-500 whitespace-nowrap">
             <tr>
-            <th scope="col" class="px-2 py-4">Accion</th>
             <th scope="col" class="px-2 py-4">Agente</th>
             <th scope="col" class="px-2 py-4">Posición</th>
             <th scope="col" class="px-2 py-4">Fila activa</th>
@@ -12,7 +11,6 @@
         <tbody>
             @forelse ($usuarios as $usuario)
             <tr class="border-b dark:border-neutral-500">
-                <td class="whitespace-nowrap px-2 py-1 font-semibold"><a class="hover:text-blue-400" href="#">Pedir numero</a></td>
                 <td class="whitespace-nowrap px-2 py-1 font-medium">{{$usuario->name}}</td>
                 <td class="whitespace-nowrap px-2 py-1">{{ isset($usuario) ? $usuario->positions->position : '' }}</td>
                 <td class="whitespace-nowrap px-2 py-1">{{ $usuario->numeros->filas->filas ?? 'Sin asignar' }}</td>
